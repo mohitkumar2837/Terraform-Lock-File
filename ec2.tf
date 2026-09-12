e13620ebff28abfebcdc40ed31159ef123fe6590
Your Name <mohitkumr17082004@gmail.com>
2026-09-13 04:51:05 +0530

    new changes

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,22 @@
+
+
+provider "aws" {
+  region = var.aws_region
+}
 resource "aws_key_pair" "ec2_key" {
   key_name   = "ec2-key"
   public_key = file(pathexpand("~/.ssh/ec2-key.pub"))
 }
 
-resource "aws_instance" "my_ec21" {
+
+
+resource "aws_instance" "my_ec2" {
+  count         = 2    
   ami           = var.ami_id
   instance_type = var.instance_type
 
   key_name = aws_key_pair.ec2_key.key_name
+  user_data = file("script.sh")
 
   vpc_security_group_ids = [
     aws_security_group.ec2_sg.id
---
+++ output.tf
@@ -0,0 +1,14 @@
+output "ec2_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.instance_type[*].public_ip
+}
+
+output "ec2_public_dns" {
+  description = "DNS address of the EC2 instance"
+  value       = aws_instance.instance_type[*].public_dns
+}
+
+output "ec2_private_ip" {
+  description = "private IP address of the EC2 instance"
+  value       = aws_instance.my_ec2[*].private_ip
+}
