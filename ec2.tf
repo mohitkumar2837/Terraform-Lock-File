b8fe12319df6b4c744a5b0f05bb3383ef7dec404
Your Name <mohitkumr17082004@gmail.com>
2026-09-13 04:28:54 +0530

    changed ec2 name

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_key_pair" "ec2_key" {
   public_key = file(pathexpand("~/.ssh/ec2-key.pub"))
 }
 
-resource "aws_instance" "my_ec2" {
+resource "aws_instance" "my_ec21" {
   ami           = var.ami_id
   instance_type = var.instance_type
 
