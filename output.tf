d328542697465c60711755c8b371d4bca88659ac
Your Name <mohitkumr17082004@gmail.com>
2026-09-13 06:25:49 +0530

    for_each output file changes

--- output.tf
+++ output.tf
@@ -1,14 +1,31 @@
+# output "ec2_public_ip" {
+#   description = "Public IP address of the EC2 instance"
+#   value       = aws_instance.my_ec2[*].public_ip
+# }
+
+# output "ec2_public_dns" {
+#   description = "DNS address of the EC2 instance"
+#   value       = aws_instance.my_ec2[*].public_dns
+# }
+
+# output "ec2_private_ip" {
+#   description = "private IP address of the EC2 instance"
+#   value       = aws_instance.my_ec2[*].private_ip
+# }
+
 output "ec2_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.my_ec2[*].public_ip
+  value = [  
+    for instance in aws_instance.my_ec2 : instance.public_ip
+]
 }
-
-output "ec2_public_dns" {
-  description = "DNS address of the EC2 instance"
-  value       = aws_instance.my_ec2[*].public_dns
-}
-
 output "ec2_private_ip" {
-  description = "private IP address of the EC2 instance"
-  value       = aws_instance.my_ec2[*].private_ip
+  value = [  
+    for instance in aws_instance.my_ec2 : instance.private_ip
+]
+  
+}
+output "ec2_public_dns" {
+  value = [  
+    for instance in aws_instance.my_ec2 : instance.public_dns
+]
 }
