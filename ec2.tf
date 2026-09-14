9101f436ef893dd8d017cc2ef708c71fc0efab3f
Your Name <mohitkumr17082004@gmail.com>
2026-09-14 21:46:03 +0530

    added security group in ec2

--- ec2.tf
+++ ec2.tf
@@ -8,6 +8,38 @@ resource "aws_key_pair" "ec2_key" {
 resource aws_default_vpc default {
 }
 
+resource aws_security_group my_security_group  {
+  name        = "terraform-ec2-sg"
+  vpc_id      = aws_default_vpc.default.id  # interpolation
+  description = "Security group for Terraform EC2"
+}
+
+# Inbound & Outbount port rules
+
+  resource aws_vpc_security_group_ingress_rule allow_http  {
+    security_group_id = aws_security_group.my_security_group.id
+    cidr_ipv4         = "0.0.0.0/0"
+    from_port         = 80
+    ip_protocol       = "tcp"
+    to_port           = 80
+  }
+
+ 
+resource aws_vpc_security_group_ingress_rule allow_ssh {
+  security_group_id = aws_security_group.my_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+
+resource aws_vpc_security_group_egress_rule allow_all_traffic {
+  security_group_id = aws_security_group.my_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
 
 
 resource "aws_instance" "my_ec2" {
