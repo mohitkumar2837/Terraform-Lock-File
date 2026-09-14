a464f58dd4980b3b4e87be0a4e53cec0fa694f09
Your Name <mohitkumr17082004@gmail.com>
2026-09-14 22:41:46 +0530

    added new vpcwrerdee

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket         = "${var.my_environment}-test-my-app-bucket-d-2026"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "${var.my_environment}-test-my-app-table-d"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
